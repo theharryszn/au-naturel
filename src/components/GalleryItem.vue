1ce13d9a77c03c66ca95d13d71fa5b17d17a6c98
Abidemi Ope <opeabidemi@gmail.com>
2023-04-19 15:07:19 +0100

    changes

---
+++ src/components/GalleryItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  image: {
+    type: String,
+    required: true
+  }
+})
+
+const speed = Math.floor(Math.random() * 3)
+const col = Math.floor(Math.random() * 3)
+</script>
+<template>
+  <div data-scroll :data-scroll-speed="col" :class="`flex flex-col relative group col-span-${col}`">
+    <img
+      :src="'/src/assets/images/gallery/' + image"
+      class="bg-zinc-800/10 object-cover border border-transparent rounded-sm hover:border-zinc-50/10 transition-all duration-700 group-hover:scale-[1.01] grayscale group-hover:grayscale-0"
+    />
+    <div class="py-2 transition text-sm md:opacity-0 group-hover:opacity-100">
+      @{{ props.image.substring(0, image.indexOf('/')) }}
+    </div>
+  </div>
+</template>
