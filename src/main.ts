e16d3d469c37832e489fbe73f0a21098f4b9507b
Abidemi Ope <opeabidemi@gmail.com>
2024-06-05 02:24:17 +0100

    mad changes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 
 import App from './App.vue'
 import router from './router'
@@ -18,3 +19,4 @@ app.use(init)
 app.use(locomotiveScroll)
 
 app.mount('#app')
+inject()
